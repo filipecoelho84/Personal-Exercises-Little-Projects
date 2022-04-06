02850c73f4abb51609f2aa45cef0b180e3665861
Filipe Coelho <coelho.sox@gmail.com>
2022-04-07 00:16:35 +0100

    writing tests for the calculator app

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -7,3 +7,15 @@ package calculator
 func Add(a, b float64) float64 {
 	return a + b
 }
+
+// Subtract takes two numbers a and b, and
+// returns the result of subtracting a from b.
+func Subtract(a, b float64) float64 {
+	return a - b
+}
+
+// Multiply takes two numbers a and b, and
+// returns the result of Multiplying a for b.
+func Multiply(a, b float64) float64 {
+	return a * b
+}

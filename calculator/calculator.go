d082908b3ebb80e057f45ce43c1ac90d9ea4b166
Filipe Coelho <coelho.sox@gmail.com>
2022-04-16 06:39:25 +0100

    Writing the behavior for error dividing one number by 0

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -2,6 +2,8 @@
 // calculations in Go.
 package calculator
 
+import "errors"
+
 // Add takes two numbers and returns the result of adding
 // them together.
 func Add(a, b float64) float64 {
@@ -20,6 +22,13 @@ func Multiply(a, b float64) float64 {
 	return a * b
 }
 
+// Divide takes two numbers and returns the
+// result of dividing the first by the
+// second, or an error if the second value
+// is zero.
 func Divide(a, b float64) (float64, error) {
-	return a / b, nil
+	if b == 0 {
+		return 0, errors.New("division by zero not allowed")
 	}
+	return a / b, nil
+}
--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -11,7 +11,7 @@ func TestAdd(t *testing.T) {
 		a, b float64
 		want float64
 	}
-	
+
 	testCases := []testCase{
 		{a: 2, b: 2, want: 4},
 		{a: 1, b: 1, want: 2},
@@ -74,7 +74,7 @@ func TestDivide(t *testing.T) {
 		a, b float64
 		want float64
 	}
-	
+
 	testCases := []testCase{
 		{a: 2, b: 2, want: 1},
 		{a: -1, b: -1, want: 1},
@@ -89,5 +89,13 @@ func TestDivide(t *testing.T) {
 		if tc.want != got {
 			t.Errorf("Divide(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
 		}
+	}
+}
+
+func TestDivideInvalid(t *testing.T) {
+	t.Parallel()
+	_, err := calculator.Divide(1, 0)
+	if err == nil {
+		t.Error("Divide(1, 0): want error for invalid input, got nil")
 	}
 }
